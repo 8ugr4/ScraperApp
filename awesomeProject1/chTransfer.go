4c7e20ac5f869bb32695648bf760bb29b2d29ab3
8ugr4 <117930037+8ugr4@users.noreply.github.com>
2024-07-04 17:39:04 +0200

    fixed formatting.
    removed DebugOutCh and DebugInCh functions. Added CountRows. formatting and variable name changes.

--- awesomeProject1/chTransfer.go
+++ awesomeProject1/chTransfer.go
@@ -10,31 +10,33 @@ import (
 	"sync"
 )
 
-// using distinct will unique the column data
-
 const (
 	//inputTableCreateSQLQuery   = `CREATE TABLE IF NOT EXISTS InputTable(url String)engine = MergeTree()order by url`
-	selectColumnSQLQuery       = "select distinct url from InputTable"
-	dropTableSQLQuery          = "DROP TABLE IF EXISTS OutputTable"
-	outputTableSQLQuery        = "CREATE TABLE IF NOT EXISTS OutputTable(url String, status String,body_length Int)engine = MergeTree()order by status"
-	insertOutputTableSQLQuery  = "INSERT INTO OutputTable (url,status,body_length) VALUES (?, ?, ?) "
-	controlOutputTableSQLQuery = "select * from OutputTable"
+	selectColumnSQLQuery      = "select distinct url from InputTable"
+	dropTableSQLQuery         = "DROP TABLE IF EXISTS OutputTable"
+	outputTableSQLQuery       = "CREATE TABLE IF NOT EXISTS OutputTable(url String, status String,body_length Int)engine = MergeTree()order by status"
+	insertOutputTableSQLQuery = "INSERT INTO OutputTable (url,status,body_length) VALUES (?, ?, ?) "
 )
 
 type chTransfer struct {
 	*URL
 }
 
-func (ct *chTransfer) CountURLinCh(conn driver.Conn) int {
+// Counts the rows of the table, could definitely be improved using another built-in function from driver.Conn.
+
+func (ct *chTransfer) CountRows(conn driver.Conn) int {
 
 	ctx := context.Background()
 
 	rows, err := conn.Query(ctx, fmt.Sprintf("%s", selectColumnSQLQuery))
+
 	if err != nil {
 		fmt.Printf("couldn't select from table: %q", err)
 	}
+
 	defer rows.Close()
 	i := 0
+
 	for rows.Next() {
 		i++
 		var url string
@@ -42,8 +44,11 @@ func (ct *chTransfer) CountURLinCh(conn driver.Conn) int {
 			log.Printf("couldn't scan the rows :%v \n", err)
 		}
 	}
+
 	return i
 }
+
+// reads the rows from the table. sends them to be parsed.
 
 func (ct *chTransfer) readFromCh(conn driver.Conn, inChan chan string) error {
 
@@ -66,16 +71,9 @@ func (ct *chTransfer) readFromCh(conn driver.Conn, inChan chan string) error {
 	return nil
 }
 
-func (ct *chTransfer) ControlOutput(conn driver.Conn) {
-	ctx := context.Background()
+// reads the parsed URL's as struct, INSERT's into OutputTable in Ch.
 
-	err := conn.Exec(ctx, fmt.Sprintf("%s", controlOutputTableSQLQuery))
-	if err != nil {
-		fmt.Printf("couldn't select from output table: %q \nor given table is empty", err)
-	}
-
-}
-func (ct *chTransfer) DebugOutCh(wg *sync.WaitGroup, conn driver.Conn, ch1 chan *Response, count int) error {
+func (ct *chTransfer) writeIntoCh(wg *sync.WaitGroup, conn driver.Conn, ch1 chan *Response, count int) error {
 
 	ctx := context.Background()
 	defer wg.Done()
@@ -88,49 +86,22 @@ func (ct *chTransfer) DebugOutCh(wg *sync.WaitGroup, conn driver.Conn, ch1 chan 
 	if err != nil {
 		return fmt.Errorf("couldn't create table: %q \n", err)
 	}
-	fmt.Println("table created successfully.")
 
-	//defer close(ch1)
-	err1 := conn.Exec(ctx, "INSERT into OutputTable (url,status,body_length) values ('www.oyle.com','ok',22325)")
+	err1 := conn.Exec(ctx, "INSERT into OutputTable (url,status,body_length) values ('www.testTESTtest.com','1234 okOK',1230321)")
 	if err1 != nil {
 		return fmt.Errorf("couldn't insert into table %q\n", err1)
 	}
 
-	for i := 0; i < count-1; i++ {
-
-		fmt.Printf("count :%d\n", count)
+	for i := 0; i < count; i++ {
 
 		Parsed := <-ch1
-
-		fmt.Printf("%s\n %s\n %d\n", Parsed.Url, Parsed.Status, Parsed.Length)
-
-		//values := fmt.Sprintf(" '%s' '%s' %d", Parsed.Url, Parsed.Status, Parsed.Length)
-		//query := insertOutputTableSQLQuery + values
 
 		err := conn.Exec(ctx, insertOutputTableSQLQuery, Parsed.Url, Parsed.Status, Parsed.Length)
 		if err != nil {
 			return fmt.Errorf("\ncould not append to the Table,\nERROR: : %q\n", err)
 		}
 
-		//fmt.Printf("url: %q\nstatus: %s\nlength: %d\n", Parsed.Url, Parsed.Status, Parsed.Length)
-
-		//batch, err := conn.PrepareBatch(ctx, fmt.Sprintf("%s", insertOutputTableSQLQuery))
-		//if err != nil {
-		//	return fmt.Errorf("couldn't prepare batch: %q \n", err)
-		//}
-
-		//err1 := batch.Append(Parsed.Url, Parsed.Status, Parsed.Length)
-		//if err1 != nil {
-		//	return fmt.Errorf("couldn't append to batch: %q \n", err)
-		//}
 	}
-
-	return fmt.Errorf("ok")
-}
-
-func (ct *chTransfer) writeIntoCh(wg *sync.WaitGroup, conn driver.Conn, ch1 chan *Response, count int) error {
-	err := ct.DebugOutCh(wg, conn, ch1, count)
-	fmt.Printf("debug into ch worked:%q\n", err)
 
 	return nil
 }
@@ -140,8 +111,7 @@ func (u *URL) connect() (driver.Conn, error) {
 	ctx := context.Background()
 
 	host, port := u.chHost, u.chPort
-	//b := string(fmt.Sprintf("%s:%s", host, port))
-	//fmt.Printf(b) : localhost:9000
+
 	conn, err := clickhouse.Open(&clickhouse.Options{
 		Addr: []string{fmt.Sprintf("%s:%s", host, port)},
 		Auth: clickhouse.Auth{
@@ -150,7 +120,7 @@ func (u *URL) connect() (driver.Conn, error) {
 			Password: "",
 		},
 		Debug: true,
-		Debugf: func(format string, v ...interface{}) { // will be executed if Debug is true
+		Debugf: func(format string, v ...interface{}) {
 			//log.Printf("debug : "+format, v...)
 		},
 	})
